fc55b1464bce1d7848ebf0e45683b679cdb39b9a
satyamraj <satyamraj-creator@users.noreply.github.com>
2020-04-29 12:36:38 +0530

    Initial commit

---
+++ lib/login_page.dart
@@ -0,0 +1,14 @@
+import"package:flutter/material.dart";
+
+class LoginPage extends StatelessWidget {
+ static const routeName='loginPage';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+          body: Center(child:
+        Text('Sirsho\'s Login Screen with green theme',style: TextStyle(fontSize: 30),),
+        
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:devhack_1/login_page.dart';
+import 'package:flutter/material.dart';
+import 'splash_screen.dart';
+void main(){
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+      routes: {
+        LoginPage.routeName:(ctx)=>LoginPage(),
+      },
+    );
+  }
+}
